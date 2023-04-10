89564ec2180cfc496ee1e2467c0e18acd4a29c98
João Victor <jvfernandes.contato@gmail.com>
2023-04-10 15:14:26 -0300

    feat: create global styles

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,6 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from './assets/styles/Global';
+import defaultTheme from './assets/styles/themes/default';
+
 function App() {
   return (
-    <h1>React-dnd</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>React-dnd</h1>
+    </ThemeProvider>
   );
 }
 
